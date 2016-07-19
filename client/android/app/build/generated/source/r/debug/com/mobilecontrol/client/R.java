3921ffb55e6b872309ad354a190e998b58dc8427
Tony Gao <tony.gao000@gmail.com>
2016-07-20 00:26:40 +0800

    add gitignore

--- client/android/app/build/generated/source/r/debug/com/mobilecontrol/client/R.java
+++ client/android/app/build/generated/source/r/debug/com/mobilecontrol/client/R.java
@@ -18,12 +18,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_connect=0x7f080003;
-        public static final int action_disconnect=0x7f080005;
-        public static final int action_send=0x7f080004;
-        public static final int action_settings=0x7f080002;
-        public static final int connecting_spinner=0x7f080000;
-        public static final int disconnected=0x7f080001;
+        public static final int action_connect=0x7f080007;
+        public static final int action_disconnect=0x7f080009;
+        public static final int action_send=0x7f080008;
+        public static final int action_settings=0x7f080006;
+        public static final int btn_left=0x7f080004;
+        public static final int btn_right=0x7f080005;
+        public static final int buttons=0x7f080001;
+        public static final int connecting_spinner=0x7f080002;
+        public static final int disconnected=0x7f080003;
+        public static final int touch_pad=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
